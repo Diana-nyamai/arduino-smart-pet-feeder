11540eadc1d9615df14e07e80a71c4dd14a9ee05
Diana-nyamai <dnyamai.dn@gmail.com>
2026-02-04 04:24:15 +0300

    feat: add RTC component

--- src/smart_pet_feeder.cpp
+++ src/smart_pet_feeder.cpp
@@ -1,13 +1,16 @@
 #include <Arduino.h>
 #include <Stepper.h>
+#include <RTClib.h>
 
 const int stepsPerRevolution = 2048; 
 Stepper feederMotor(stepsPerRevolution, 8, 9, 10, 11); // initialize the stepper on pins 8 through 11
+RTC_DS3231 rtc;
 
 // feeding settings
 const int feedSpeed = 8;
 const int smallPortion = 512;
 const int largePortion = 1024;
+
 
 // To prevent motor overheating, we will release the motor coils after each feeding
 void releaseMotorCoils()
@@ -20,6 +23,16 @@ void releaseMotorCoils()
 
 void setup() {
   Serial.begin(9600);
+
+  if (!rtc.begin()) {
+    Serial.println("Couldn't find RTC");
+    while (1);
+  }
+
+  if (rtc.lostPower()) {
+    Serial.println("RTC lost power, setting the time!");
+    rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+  }
 
   // set motor control pins as outputs
   pinMode(8, OUTPUT);
@@ -57,8 +70,17 @@ void feedCat(int portionSteps){
 }
 
 void loop() {
+  DateTime now = rtc.now();
+
+  Serial.print("Current time: ");
+  Serial.print(now.hour());
+  Serial.print(":");
+  Serial.print(now.minute());
+  Serial.print(":");
+  Serial.println(now.second());
+
   Serial.println("Feeding pet...");
-  // feederMotor.step(stepsPerRevolution); // Rotate one full revolution
+  Serial.print("Time:");
   feedCat(smallPortion); // Dispense small portion
   delay(10000); // Wait for 10 seconds before next feeding
   releaseMotorCoils(); // Release motor coils after stepping
